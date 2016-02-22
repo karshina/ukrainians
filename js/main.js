0abbcaf6e3bfe81e7007531975fcc4b7fe28939e
Aliya Karshina <aliya.karshina@gmail.com>
2016-02-22 13:24:42 +0200

    add trailer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-$('#play').click(function(){
-$('video')[0].play();
+$(document).ready(function($) {
+	var $window = $(window)
+	var $play = $('.play')
+	console.warn("HUY", $play.length)
+	var $video = $('.video')
+    var player = {}
+
+$play.on('click', function () {
+  $('.video').removeClass("none");
+  $('iframe').playVideo();
 })
 
 function retinaSrc(src) {
@@ -8,3 +16,12 @@ function retinaSrc(src) {
     }
     return src
   }
+
+$window.resize(function() {
+    if (!player.h) return
+    player.h.h.width = $window.width()
+    player.h.h.height = $window.height()
+})
+
+
+})
