b724e7debc6f4a7c615117ad784f72e35e08d1e8
Aliya Karshina <aliya.karshina@gmail.com>
2016-03-13 21:39:54 +0200

    full screen for video

--- js/main.js
+++ js/main.js
@@ -1,27 +1,66 @@
 $(document).ready(function($) {
 	var $window = $(window)
 	var $play = $('.play')
-	console.warn("HUY", $play.length)
 	var $video = $('.video')
-    var player = {}
+	var $close = $('.close')
+	var $lang = $('.lang')
+	var $intro = $('.intro')
+	var $footer = $('.footer')
+	var player = {}
 
-$play.on('click', function () {
-  $('.video').removeClass("none");
-  $('iframe').playVideo();
-})
+    var tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 function retinaSrc(src) {
     if (window.Retina.isRetina()) {
-      src = src.replace('.jpg', '@2x.jpg')
+      src = src.replace('.jpg', '@2x.jpg');
     }
-    return src
+    return src;
   }
 
 $window.resize(function() {
-    if (!player.h) return
-    player.h.h.width = $window.width()
-    player.h.h.height = $window.height()
+    if (!player.h) return;
+    player.h.h.width = $window.width();
+    player.h.h.height = $window.height();
 })
 
+$play.on('click', function () {
+  $video.removeClass('none');
+  $play.addClass('none');
+  $lang.addClass('none');
+  $intro.addClass('none');
+  $footer.addClass('none');
+  $close.removeClass('none');
+  onYouTubeIframeAPIReady();
+})
+
+$close.on('click', function () {
+  location.reload()
+  // $video.addClass('none');
+  // $play.removeClass('none');
+  // $lang.removeClass('none');
+  // $footer.removeClass('none');
+  // $intro.removeClass('none');
+  // $close.addClass('none');
+  // player.stopVideo();
+  // player.clearVideo();
+})
+
+
+function onYouTubeIframeAPIReady () {
+  player = new YT.Player('player', {
+    height: '100%',
+    width: '100%',
+    videoId: 'Fftqr09RQ1w',
+    events: {
+      'onReady': function(event) {
+          event.target.playVideo()
+        }
+    }
+  });
+}
 
 })
